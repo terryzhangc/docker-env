23a699abfd611b8c43adec78ee7f487753bdae90
zhangchao6 <zhangchao6@faw.com.cn>
2019-11-18 20:44:55 +0800

    1

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,9 +1,14 @@
 echo `service mysql status`
 
 echo '开始导入数据....'
+
+echo $MYSQL_ROOT_PASSWORD
+echo $MYSQL_ROOT_HOST
 #导入数据
-mysql < /mysql/sakila-schema.sql
-mysql < /mysql/sakila-data.sql
+mysql -uroot < /mysql/sakila-schema.sql
+
+mysql -uroot < /mysql/sakila-data.sql
+
 echo '导入数据完毕....'
 
 echo `service mysql status`
